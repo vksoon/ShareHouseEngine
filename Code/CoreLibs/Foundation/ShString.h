eb5f795c1296ced317ad5e411025e2ea1b6ff092
eric <vkmskim@gmail.com>
2021-06-07 21:53:49 +0900

    Added Temporary StreamString. be replaced sh-string.

--- Code/CoreLibs/Foundation/ShString.h
+++ Code/CoreLibs/Foundation/ShString.h
@@ -8,4 +8,8 @@ template <typename _Type>
 using BasicString = std::basic_string < _Type, std::char_traits<_Type>, StdAlloc<_Type> >;
 using String = BasicString<char>;
 
+template <typename _Type>
+using BasicStringStream = std::basic_stringstream < _Type, std::char_traits<_Type>, StdAlloc<_Type> >;
+using StringStream = BasicStringStream<char>;
+
 SH_NAMESPACE_END
